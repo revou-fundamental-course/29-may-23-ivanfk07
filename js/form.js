fc7a65ed0d462b9849e253d0df3c6689e595c51d
ivanfk07 <119605237+ivanfk07@users.noreply.github.com>
2023-06-08 20:28:22 +0700

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,15 +1,19 @@
 function validateForm() {
     var name = document.forms["message-form"]["full-name"].value;
+    var email = document.forms["message-form"]["email"].value;
+    var phoneNumber = document.forms["message-form"]["phone-number"].value;
     var date = document.forms["message-form"]["birth-date"].value;
     var gender = document.forms["message-form"]["gender"].value;
     var messages = document.forms["message-form"]["messages"].value;
 
-    if (name == "" || date == "" || gender == "" || messages == "") {
+    if (name == "" || date == "" || gender == "" || messages == "" || email == "" || phoneNumber == "") {
         alert("Form Harus Di Isi Semua");
         return false;
     }
 
     document.getElementById("sender-full-name").innerText = name;
+    document.getElementById("sender-email").innerText = email;
+    document.getElementById("sender-phone-number").innerText = phoneNumber;
     document.getElementById("sender-birth-date").innerText = date;
     document.getElementById("sender-gender").innerText = gender;
     document.getElementById("sender-messages").innerText = messages;
